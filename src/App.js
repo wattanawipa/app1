1a73e2d5f5891f8f6d84faa2f5a9140951445672
wattanawipa <minn7714@gmail.com>
2024-12-25 15:32:22 +0700

    add  Class-component.ls file
    add  Class-component.ls file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React  from "react"
 import { Content, Footer, Header } from "./func-components";
+import Calendar from "./class-component";
 
 
 
 function App() {
   return (
-  [<Header/>,<Content/>,<Footer/>]
+    <>
+      <Header/>
+      <p><center><Calendar/></center></p>
+      <Content/>
+      <Footer/>
+    </>
   );
 
 }
+
 
 export default App;

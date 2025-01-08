055c7c5d6361c3bc8fed8521978ce07fdd6ddbdb
wattanawipa <minn7714@gmail.com>
2025-01-08 15:25:32 +0700

    add calculator
    add calculator

--- src/calculator.js
+++ src/calculator.js
@@ -5,9 +5,9 @@ export function Calculator2(){
     const n2 = 4
 
     const calculate = (op) =>{
-        let c = '${n1} ${op} ${n2}'
+        let c = `${n1} ${op} ${n2}`
         let r = eval(c)
-        alert('${c} = ${r}')
+        alert(`${c} = ${r}`)
     }
     return(
         <div style={{textAlign:'center',marginTop:20}}>
